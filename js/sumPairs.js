fb32126dcaaed15ec5690c78e543ada956f79a8a
Chris <chrisvolar@gmail.com>
2022-05-30 20:49:21 -0500

    test case need work

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,19 @@
-exports.sumPairs = function() {
-
+exports.sumPairs = function(array,target) {
+    let sumPairArray = [];
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            //console.log(array[i] + array[j])
+            if(array[i] + array[j] === target) {
+                sumPairArray.push([array[i],array[j]])
+                delete array[i],array[j];
+                
+            }
+        }
+    }
+    if(sumPairArray.length === 0) {
+        return "unable to find pairs"
+    }
+    return sumPairArray;
 };
+
+console.log(exports.sumPairs([3, 1, 5, 8, 2], 27));
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,8 @@
 var sp = require("./sumPairs");
+var shallowEqualArrays = require('shallow-equal/arrays');
 
 // Don't forget to add your tests :)
+// console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(8)), [0, 1, 2, 3, 4, 5, 6, 7]));
+console.log(shallowEqualArrays(sp.sumPairs([1,2,3,4,5], 9)),[ [4,5] ]);
+console.log(shallowEqualArrays(sp.sumPairs([1,2,3,4,5], 7)),[ [2,5], [3,4] ]);
+console.log(shallowEqualArrays(sp.sumPairs([3, 1, 5, 8, 2], 27)),'unable to find pairs');
